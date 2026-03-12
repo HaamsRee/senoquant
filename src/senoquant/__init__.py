6d72bf9396127a8467633ea0a03846d34abc2e99
HaamsRee <li.yukang@mayo.edu>
2026-03-11 23:01:45 -0500

    Prepare beta11 release

--- src/senoquant/__init__.py
+++ src/senoquant/__init__.py
@@ -4,7 +4,7 @@ try:
     from importlib.metadata import version
     __version__ = version("senoquant")
 except Exception:
-    __version__ = "1.0.0b10"  # Fallback for development
+    __version__ = "1.0.0b11"  # Fallback for development
 
 from ._widget import SenoQuantWidget
 __all__ = ["SenoQuantWidget"]
